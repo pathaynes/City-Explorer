e8ef0b5431bb189086a363a880c4579101668f2d
Dave Trost <davetrost@gmail.com>
2019-08-20 14:01:59 -0700

    weather hooked up but date formatting is stuck

--- server.js
+++ server.js
@@ -13,10 +13,23 @@ const PORT = process.env.PORT;
 // - enable CORS
 app.use(cors());
 
+app.use(express.static('server.js'));
+
 app.get('/location', (request, response) => {
     try {
         const responseObject = getLatLong(request.query.search);
         response.status(200).send(responseObject);
+    } 
+    catch(err) {
+        response.status(500).send({ error: 'something blew up' });
+    }
+});
+
+app.get('/weather', (request, response) => {
+    try {
+        console.log(request.location);
+        const responseArray = getWeather(request.location);
+        response.status(200).send(responseArray);
     } 
     catch(err) {
         response.status(500).send({ error: 'something blew up' });
@@ -32,6 +45,26 @@ function getLatLong(searchString){
         'longitude': geoData.results[0].geometry.location.lng
     };
     return responseObject;
+}
+
+const skyNet = require('./data/darksky.json');
+function getWeather(location) {
+    const forecastArray = [];
+    skyNet.daily.data.forEach(dailyForecast => {
+        const forecastDay = new Date(dailyForecast.time);
+        const dateArray = forecastDay.toUTCString().split(' ');
+        let day = dateArray[0].split(',')[0];
+        let date = dateArray[1];
+        let month = dateArray[2];
+        let year = dateArray[3];
+        const responseObject = {
+            'forecast': dailyForecast.summary,
+            'time': [day, month, date, year].join(' '),
+        };
+        forecastArray.push(responseObject);
+    });
+    console.log(forecastArray);
+    return forecastArray;
 }
 
 // Start the server
